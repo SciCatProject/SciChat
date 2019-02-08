f60ec8b7923622952111d62900bd87651254cf56
Henrik Johansson <henrik.johansson712@gmail.com>
2019-02-08 16:11:26 +0100

    Added function for searching chat log by date

--- connect-to-matrix.js
+++ connect-to-matrix.js
@@ -1,5 +1,7 @@
-let sdk = require('matrix-js-sdk');
-let authData = require('./AuthData');
+'use strict';
+
+const sdk = require('matrix-js-sdk');
+const authData = require('./AuthData');
 
 let baseUrl = 'https://matrix.org';
 let accessToken = authData.accessToken;
@@ -17,38 +19,53 @@ client.startClient({
 
 client.once('sync', function (state, prevState, res) {
     if (state === 'PREPARED') {
-        console.log("prepared");
+        console.log('prepared');
+        // let rooms = client.getRooms();
+        // let thisThing = rooms[0].getLiveTimeline();
+        // thisThing.forEach(matrixEvent => {
+        //     console.log(matrixEvent.event.content.body);
+        // });
+        // console.log(thisThing);
+        // console.log(rooms[0].getTimelineSets());
+        // let timelineWindow = sdk.TimelineWindow(client, rooms[0].getTimelineSets());
+        // console.log(timelineWindow.canPaginate(sdk.EventTimeline.BACKWARDS));
+
     } else {
         console.log(state);
         process.exit(1);
     }
 });
 
-client.on('Room.timeline', function (event, room, toStartOfTimeline) {
-    if (event.getType() !== 'm.room.message') {
-        return;
-    }
-
-    // let roomId = event.event.room_id;
-    // let botResponse = 'Measurements are now ready.';
-    // if (event.getRoomId() === roomId && event.getContent().body[0] === '!') {
-    //     sendNotice(botResponse, roomId);
-    // }
+    // let rooms = client.getRooms();
+    // client.paginateEventTimeline(rooms[0].timeline, {
+    //     backwards: true,
+    //     limit: 1000
+    // });
 
 
-    // console.log(event.event);
-    // console.log(room.name);
-    let messageTimeStamp = new Date(Date.now() - event.event.unsigned.age);
-    messageTimeStamp.setUTCHours(messageTimeStamp.getUTCHours() + 1);
-    let messageTimeStampSplit = messageTimeStamp.toISOString().split(/[T.]+/);
-    let sender = event.event.sender.split(/[@:]+/)[1];
+// showAllMessages();
 
-    if (event.event.sender === userId) {
-        console.log(`[${messageTimeStampSplit[0]}, ${messageTimeStampSplit[1]}] ${sender} >>> ${event.event.content.body}`);
-    } else {
-        console.log(`[${messageTimeStampSplit[0]}, ${messageTimeStampSplit[1]}] ${sender} <<< ${event.event.content.body}`);
-    }
-});
+findByDate('04 Feb 2019');
+
+function showAllMessages() {
+    client.on('Room.timeline', function (event, room, toStartOfTimeline) {
+        if (event.getType() !== 'm.room.message') {
+            return;
+        }
+
+        // let roomId = event.event.room_id;
+        // let botResponse = 'Measurements are now ready.';
+        // if (event.getRoomId() === roomId && event.getContent().body[0] === '!') {
+        //     sendNotice(botResponse, roomId);
+        // }
+
+
+        // console.log(event.event);
+        // console.log(room.name);
+
+        printChatLog(room);
+    });
+}
 
 function sendNotice(body, roomId) {
     let content = {
@@ -58,6 +75,38 @@ function sendNotice(body, roomId) {
     client.sendEvent(roomId, 'm.room.message', content, '', (err, res) => {
         if (err) {
             console.log(err);
+        }
+    });
+}
+
+function findByDate(date) {
+    client.on('Room.timeline', function (event, room, toStartOfTimeline) {
+        if (event.getType() !== 'm.room.message') {
+            return;
+        }
+
+        let requestDate = new Date(date);
+        let messageAge = new Date(Date.now() - event.event.unsigned.age);
+        messageAge.setHours(0, 0, 0, 0);
+
+        if (messageAge.getTime() === requestDate.getTime()) {
+            printChatLog(room);
+        }
+    });
+}
+
+function printChatLog(room) {
+    let events = room.getLiveTimeline().getEvents();
+    events.forEach(event => {
+        let messageTimeStamp = new Date(Date.now() - event.event.unsigned.age);
+        messageTimeStamp.setUTCHours(messageTimeStamp.getUTCHours() + 1);
+        let messageTimeStampSplit = messageTimeStamp.toISOString().split(/[T.]+/);
+        let sender = event.event.sender.split(/[@:]+/)[1];
+
+        if (event.event.sender === userId) {
+            console.log(`[${messageTimeStampSplit[0]}, ${messageTimeStampSplit[1]}] ${sender} >>> ${event.event.content.body}`);
+        } else {
+            console.log(`[${messageTimeStampSplit[0]}, ${messageTimeStampSplit[1]}] ${sender} <<< ${event.event.content.body}`);
         }
     });
 }
